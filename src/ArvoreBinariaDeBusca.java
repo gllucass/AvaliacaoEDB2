e0cbe8d5646178f6c94368cad49ba3c15fba9f9a
gllucass <geraldolucas10@gmail.com>
2024-08-10 20:41:26 -0300

    Iniciando projeto

---
+++ src/ArvoreBinariaDeBusca.java
@@ -0,0 +1,12 @@
+public class ArvoreBinariaDeBusca {
+    private class No {
+        Produto produto ;
+        No esquerda , direita , pai ;
+        No( Produto produto ) {
+            this.produto = produto ;
+        }
+    }
+    private No raiz ;
+
+
+}
---
+++ src/Produto.java
@@ -0,0 +1,29 @@
+public class Produto {
+    private int codigo;
+    private String nome;
+    private int quantidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
